9c84ba3a3481ebe8f74e9f95fc68fc34431e1876
cassie0508 <chenguanyu1958@gmail.com>
2024-08-26 01:04:18 +0200

    fix bugs and create a workable demo

--- Assets/KinectPublisherCopy.cs
+++ Assets/KinectPublisherCopy.cs
@@ -17,8 +17,6 @@ namespace Kinect4Azure
         [SerializeField] private Texture2D DepthImage;
         [SerializeField] private Texture2D ColorImage;
         [SerializeField] private Texture2D ColorInDepthImage;
-        [SerializeField] private int[] Capture;
-        [SerializeField] private Texture2D LookUp;
 
         private Playback playback;
         private PublisherSocket dataPubSocket;
@@ -49,6 +47,114 @@ namespace Kinect4Azure
             }
         }
 
+        private IEnumerator CameraCapture()
+        {
+            /* Import playback file */
+            try
+            {
+                playback = new Playback("Assets/capture.mkv");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Failed to open playback file: " + ex.Message);
+                yield break;
+            }
+
+            // For debugging: Set up textures
+            SetupTextures(ref ColorImage, ref DepthImage, ref ColorInDepthImage);
+
+            /* Publish Camera Data */
+            var calibration = playback.playback_calibration;
+            byte[] xyLookupData = GenerateXYTableData(calibration);
+
+            var extrinsics = calibration.DeviceExtrinsics[(int)CalibrationDeviceType.Depth + (int)CalibrationDeviceType.Color];
+            Matrix4x4 extrinsics4x4 = new Matrix4x4();
+            extrinsics4x4.SetRow(0, new Vector4(extrinsics.Rotation[0], extrinsics.Rotation[3], extrinsics.Rotation[6], extrinsics.Translation[0] / 1000.0f));
+            extrinsics4x4.SetRow(1, new Vector4(extrinsics.Rotation[1], extrinsics.Rotation[4], extrinsics.Rotation[7], extrinsics.Translation[1] / 1000.0f));
+            extrinsics4x4.SetRow(2, new Vector4(extrinsics.Rotation[2], extrinsics.Rotation[5], extrinsics.Rotation[8], extrinsics.Translation[2] / 1000.0f));
+            extrinsics4x4.SetRow(3, new Vector4(0, 0, 0, 1));
+            byte[] calibrationData = Matrix4x4ToByteArray(extrinsics4x4);
+
+            byte[] cameraSizeData = null;
+            try
+            {
+                using (var capture = playback.GetNextCapture())
+                {
+                    int[] captureArray = new int[6] {
+                        1920, 1080,     // TODO: Have issue in capture.Color
+                        capture.Depth.WidthPixels, capture.Depth.HeightPixels,
+                        capture.IR.WidthPixels, capture.IR.HeightPixels
+                    };
+
+                    cameraSizeData = new byte[captureArray.Length * sizeof(int)];
+                    Buffer.BlockCopy(captureArray, 0, cameraSizeData, 0, cameraSizeData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogWarning("Failed to get capture: " + ex.Message);
+            }
+
+            // Data: [xyLookupData.Length][calibrationData.Length][cameraSizeData.Length]
+            //       [xyLookupData][calibrationData][cameraSizeData]
+            int cameraTotalSize = sizeof(int) * 3 + xyLookupData.Length + calibrationData.Length + cameraSizeData.Length;
+            byte[] cameraData = new byte[cameraTotalSize];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(xyLookupData.Length), 0, cameraData, 0, sizeof(int));
+            Buffer.BlockCopy(BitConverter.GetBytes(calibrationData.Length), 0, cameraData, sizeof(int) * 1, sizeof(int));
+            Buffer.BlockCopy(BitConverter.GetBytes(cameraSizeData.Length), 0, cameraData, sizeof(int) * 2, sizeof(int));
+             
+            Buffer.BlockCopy(xyLookupData, 0, cameraData, sizeof(int) * 3, xyLookupData.Length);
+            Buffer.BlockCopy(calibrationData, 0, cameraData, sizeof(int) * 3 + xyLookupData.Length, calibrationData.Length);
+            Buffer.BlockCopy(cameraSizeData, 0, cameraData, sizeof(int) * 3 + xyLookupData.Length + calibrationData.Length, cameraSizeData.Length);
+
+            PublishData("Camera", cameraData);
+
+            /* Publish Frame Data */
+            var kinectCalibration = calibration.CreateTransformation();
+
+            while (true)
+            {
+                using (var capture = playback.GetNextCapture())
+                {
+                    if (capture == null)
+                    {
+                        Debug.Log("End of playback file reached.");
+                        break;
+                    }
+
+                    // For debugging: Apply data to textures
+                    ColorImage.LoadRawTextureData(capture.Color.Memory.ToArray());
+                    ColorImage.Apply();
+                    DepthImage.LoadRawTextureData(capture.Depth.Memory.ToArray());
+                    DepthImage.Apply();
+                    ColorInDepthImage.LoadRawTextureData(kinectCalibration.ColorImageToDepthCamera(capture).Memory.ToArray());
+                    ColorInDepthImage.Apply();
+
+                    byte[] colorData = capture.Color.Memory.ToArray();
+                    byte[] depthData = capture.Depth.Memory.ToArray();
+                    byte[] colorInDepthData = kinectCalibration.ColorImageToDepthCamera(capture).Memory.ToArray();
+
+                    // Data: [colorData.Length][depthData.Length][colorInDepthData.Length]
+                    //       [colorData][depthData][colorInDepthData]
+                    int frameTotalSize = colorData.Length + depthData.Length + colorInDepthData.Length + sizeof(int) * 3;
+                    byte[] frameData = new byte[frameTotalSize];
+
+                    Buffer.BlockCopy(BitConverter.GetBytes(colorData.Length), 0, frameData, 0, sizeof(int));
+                    Buffer.BlockCopy(BitConverter.GetBytes(depthData.Length), 0, frameData, sizeof(int) * 1, sizeof(int));
+                    Buffer.BlockCopy(BitConverter.GetBytes(colorInDepthData.Length), 0, frameData, sizeof(int) * 2, sizeof(int));
+
+                    Buffer.BlockCopy(colorData, 0, frameData, sizeof(int) * 3, colorData.Length);
+                    Buffer.BlockCopy(depthData, 0, frameData, sizeof(int) * 3 + colorData.Length, depthData.Length);
+                    Buffer.BlockCopy(colorInDepthData, 0, frameData, sizeof(int) * 3 + colorData.Length + depthData.Length, colorInDepthData.Length);
+ 
+                    PublishData("Frame", frameData);
+                }
+
+                yield return null;
+            }
+        }
+
         private void SetupTextures(ref Texture2D Color, ref Texture2D Depth, ref Texture2D ColorInDepth)
         {
             try
@@ -66,134 +172,6 @@ namespace Kinect4Azure
             catch (Exception ex)
             {
                 Debug.LogWarning($"An error occurred " + ex.Message);
-            }
-        }
-
-        private IEnumerator CameraCapture()
-        {
-            try
-            {
-                playback = new Playback("Assets/capture.mkv");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"Failed to open playback file: {ex.Message}");
-                yield break;
-            }
-
-            var calibration = playback.playback_calibration;
-            var kinectCalibration = calibration.CreateTransformation();
-
-            SetupTextures(ref ColorImage, ref DepthImage, ref ColorInDepthImage);
-
-
-            // Publish camera capture information
-            try
-            {
-                using (var capture = playback.GetNextCapture())
-                {
-                    Capture = new int[6] {
-                        1920, 1080,
-                        capture.Depth.WidthPixels, capture.Depth.HeightPixels,
-                        capture.IR.WidthPixels, capture.IR.HeightPixels
-                    };
-
-                    byte[] captureData = new byte[Capture.Length * sizeof(int)];
-                    Buffer.BlockCopy(Capture, 0, captureData, 0, captureData.Length);
-                    PublishData("Capture", captureData);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogWarning($"An error occurred " + ex.Message);
-            }
-
-            byte[] xyTableData = GenerateXYTableData(calibration);
-            PublishData("XYTable", xyTableData);
-
-
-            var extrinsics = calibration.DeviceExtrinsics[(int)CalibrationDeviceType.Depth + (int)CalibrationDeviceType.Color];
-            Matrix4x4 extrinsics4x4 = new Matrix4x4();
-            extrinsics4x4.SetRow(0, new Vector4(extrinsics.Rotation[0], extrinsics.Rotation[3], extrinsics.Rotation[6], extrinsics.Translation[0] / 1000.0f));
-            extrinsics4x4.SetRow(1, new Vector4(extrinsics.Rotation[1], extrinsics.Rotation[4], extrinsics.Rotation[7], extrinsics.Translation[1] / 1000.0f));
-            extrinsics4x4.SetRow(2, new Vector4(extrinsics.Rotation[2], extrinsics.Rotation[5], extrinsics.Rotation[8], extrinsics.Translation[2] / 1000.0f));
-            extrinsics4x4.SetRow(3, new Vector4(0, 0, 0, 1));
-
-            Matrix4x4 color2DepthCalibration = extrinsics4x4;
-
-            byte[] calibrationData = Matrix4x4ToByteArray(color2DepthCalibration);
-            PublishData("Color2DepthCalibration", calibrationData);
-
-            while (true)
-            {
-                using (var capture = playback.GetNextCapture())
-                {
-                    if (capture == null)
-                    {
-                        Debug.Log("End of playback file reached.");
-                        break;
-                    }
-
-                    ColorImage.LoadRawTextureData(capture.Color.Memory.ToArray());
-                    ColorImage.Apply();
-
-                    DepthImage.LoadRawTextureData(capture.Depth.Memory.ToArray());
-                    DepthImage.Apply();
-
-                    ColorInDepthImage.LoadRawTextureData(kinectCalibration.ColorImageToDepthCamera(capture).Memory.ToArray());
-                    ColorInDepthImage.Apply();
-
-                    byte[] colorData = ColorImage.GetRawTextureData();
-                    byte[] depthData = DepthImage.GetRawTextureData();
-                    byte[] colorInDepthData = ColorInDepthImage.GetRawTextureData();
-
-                    int totalSize = colorData.Length + depthData.Length + colorInDepthData.Length + sizeof(int) * 3;
-                    byte[] allData = new byte[totalSize];
-
-                    Buffer.BlockCopy(BitConverter.GetBytes(colorData.Length), 0, allData, 0, sizeof(int));
-                    Buffer.BlockCopy(BitConverter.GetBytes(depthData.Length), 0, allData, sizeof(int), sizeof(int));
-                    Buffer.BlockCopy(BitConverter.GetBytes(colorInDepthData.Length), 0, allData, sizeof(int) * 2, sizeof(int));
-
-                    Buffer.BlockCopy(colorData, 0, allData, sizeof(int) * 3, colorData.Length);
-                    Buffer.BlockCopy(depthData, 0, allData, sizeof(int) * 3 + colorData.Length, depthData.Length);
-                    Buffer.BlockCopy(colorInDepthData, 0, allData, sizeof(int) * 3 + colorData.Length + depthData.Length, colorInDepthData.Length);
-
-                    PublishData("AllFrames", allData);
-                }
-                yield return null;
-            }
-        }
-
-        private byte[] Matrix4x4ToByteArray(Matrix4x4 matrix)
-        {
-            float[] matrixFloats = new float[16];
-            for (int i = 0; i < 16; i++)
-            {
-                matrixFloats[i] = matrix[i];
-            }
-
-            byte[] byteArray = new byte[matrixFloats.Length * sizeof(float)];
-            Buffer.BlockCopy(matrixFloats, 0, byteArray, 0, byteArray.Length);
-            return byteArray;
-        }
-
-        private void PublishData(string topic, byte[] data)
-        {
-            if (dataPubSocket != null)
-            {
-                try
-                {
-                    dataPubSocket.SendMoreFrame(topic).SendFrame(data);
-                }
-                catch (NetMQ.TerminatingException)
-                {
-                    Debug.LogWarning("Context was terminated. Reinitializing socket.");
-                    InitializeSocket();
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogWarning($"Failed to publish data: {ex.Message}");
-                }
             }
         }
 
@@ -231,10 +209,41 @@ namespace Kinect4Azure
             }
 
             xylookup.Apply();
-            LookUp = xylookup;
-
             byte[] xyTableData = xylookup.GetRawTextureData();
             return xyTableData;
+        }
+
+        private byte[] Matrix4x4ToByteArray(Matrix4x4 matrix)
+        {
+            float[] matrixFloats = new float[16];
+            for (int i = 0; i < 16; i++)
+            {
+                matrixFloats[i] = matrix[i];
+            }
+
+            byte[] byteArray = new byte[matrixFloats.Length * sizeof(float)];
+            Buffer.BlockCopy(matrixFloats, 0, byteArray, 0, byteArray.Length);
+            return byteArray;
+        }
+
+        private void PublishData(string topic, byte[] data)
+        {
+            if (dataPubSocket != null)
+            {
+                try
+                {
+                    dataPubSocket.SendMoreFrame(topic).SendFrame(data);
+                }
+                catch (NetMQ.TerminatingException)
+                {
+                    Debug.LogWarning("Context was terminated. Reinitializing socket.");
+                    InitializeSocket();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogWarning($"Failed to publish data: {ex.Message}");
+                }
+            }
         }
 
         private void OnDestroy()
